145e734ca8775b7ba1827def20b79b1f7478b37c
Serg Nakovnick <w8smile@gmail.com>
2024-05-12 17:57:01 +0300

    errors

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setErrorAC, setStatusAC } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   try {
@@ -9,7 +10,7 @@ export const fetchDecksTC = () => async (dispatch: Dispatch) => {
     const res = await decksAPI.fetchDecks()
     dispatch(setDecksAC(res.data.items))
     dispatch(setStatusAC('succeeded'))
-  } catch(e) {
+  } catch (error) {
     dispatch(setStatusAC('failed'))
   }
 }
@@ -21,13 +22,33 @@ export const addDeckTC = (name: string) => async (dispatch: Dispatch) => {
 }
 
 export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
-  return decksAPI.deleteDeck(id).then((res) => {
+  try {
+    const res = await decksAPI.deleteDeck(id)
     dispatch(deleteDeckAC(res.data.id))
-  })
+  } catch (error) {
+    let errorMessage: string
+
+    if (isAxiosError(error))
+      errorMessage = error.response
+        ? `Ошибка сервера ${error.response.data.errorMessages[0].message}`
+        : `Ошибка клиента: ${error.message}`
+    else errorMessage = `Другая ошибка: ${(error as Error).message}`
+    console.log(errorMessage)
+  }
 }
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (error) {
+    let errorMessage: string
+
+    if (isAxiosError(error))
+      errorMessage = error.response
+      ? `Ошибка сервера ${error.response.data.errorMessages[0].message}`
+      : `Ошибка клиента: ${error.message}`
+    else errorMessage = `Другая ошибка: ${(error as Error).message}`
+    console.log(errorMessage)
+  }
 }
